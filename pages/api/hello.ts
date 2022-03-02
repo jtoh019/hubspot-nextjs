5b6be73082e6f1793f36741257a253f9642f8b14
Jasmine Toh <jtoh@hubspot.local>
2022-03-02 12:43:36 +0800

    initial commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+  req: NextApiRequest, res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method != 'GET') {
+    return res.status(405).json({ message: 'This endpoint is for GET request' })
+  }
+  const contactId = req.query.contactid
+  if (!contactId) {
+    return res.status(400).json({ message: 'Please provide a contact ID' })
+  }
+  return res.status(200).json({ name: 'John Doe' })
 }
